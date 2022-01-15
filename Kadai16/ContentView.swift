90f86b4e91d6c978dda452324f96b3d56ab5af48
mana.u <great8life@gmail.com>
2022-01-15 15:38:27 +0900

    Initial Commit

---
+++ Kadai16/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kadai16
+//
+//  Created by mana on 2022/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kadai16/Kadai16App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai16App.swift
+//  Kadai16
+//
+//  Created by mana on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct Kadai16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
